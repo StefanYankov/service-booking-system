ee063a947cec566a37588be0369f09a0020da854
Stefan Yankov <syankoff@gmail.com>
2026-01-12 18:41:19 +0200

    feat(MVC): Implement Identity Authentication with Custom Fields and Email Confirmation

---
+++ ServiceBookingSystem.IntegrationTests/Identity/IdentityPagesTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace ServiceBookingSystem.IntegrationTests.Identity;
+
+public class IdentityPagesTests : BaseIntegrationTest
+{
+    public IdentityPagesTests(CustomWebApplicationFactory<Program> factory, ITestOutputHelper output) : base(factory, output)
+    {
+    }
+
+    [Theory]
+    [InlineData("/Identity/Account/Login")]
+    [InlineData("/Identity/Account/Register")]
+    public async Task Get_IdentityPage_ReturnsSuccess(string url)
+    {
+        // Act
+        var response = await this.Client.GetAsync(url);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Contains("text/html", response.Content.Headers.ContentType!.ToString());
+    }
+}
